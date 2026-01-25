7bad9a7ff89dfc041acac81430ae8ef9ff9c8fbc
Dani Guardiola <hi@daniguardio.la>
2026-01-25 14:33:51 +0100

    Update options.sh and install-utils to use a consistent remote repository URL

--- options.sh
+++ options.sh
@@ -6,3 +6,6 @@
 
 # the main user in dietpi
 MAIN_DIETPI_USER="dani"
+
+# the remote http URL of this repo, used mostly for fetching
+REMOTE_REPO_HTTP_URL="https://github.com/DaniGuardiola/home-network.git"
